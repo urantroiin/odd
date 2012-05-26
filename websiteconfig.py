72524e684d2b26ff68ad04b26d68806a3d90b2c7
xys <wsxys08@gmail.com>
2012-05-26 22:53:27 +0800

    clean

--- websiteconfig.py
+++ websiteconfig.py
@@ -16,10 +16,3 @@ SECRET_KEY = r"42a3e1376f8852d1c0620a3235886bcd712879a3"
 #DB
 DATABASE_URI = 'mysql+oursql://root:123456@localhost/odd'
 DATABASE_CONNECT_OPTIONS = {}
-
-'''
-load production environment
-'''
-from os.path import isfile
-if isfile('config.py'):
-    from config import *
