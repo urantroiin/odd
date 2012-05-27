519650cc8be779056a4b045c5f754fdc3d34a8bc
xys <wsxys08@gmail.com>
2012-05-27 19:57:22 +0800

    mv config out

--- website_config.py
+++ website_config.py
@@ -1,18 +1,1 @@
-# -*- coding: utf-8 -*-
-
-'''
-production environment
-'''
-#host
-HOST = '127.0.0.1'
-PORT = 5001
-
-#debug
-DEBUG = False
-
-#secret key
-SECRET_KEY = '/\xb4\xf5\xbe\x16\x19`\x1e@^\xa5Zi\xab;E}.\xf2~\xd2\xad\xcc\x00'
-
-#DB
-DATABASE_URI = 'mysql+oursql://odd:odd@localhost/odd'
-DATABASE_CONNECT_OPTIONS = {}
+../website_config.py
